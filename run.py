9ab4bb4d5be3244d1396e4d63e944ea1f8fe9c76
Magnus Haraldson Høie <39849954+Magnushhoie@users.noreply.github.com>
2025-12-08 11:49:21 +0100

    imm any region

--- run.py
+++ run.py
@@ -414,12 +414,10 @@ def main(args):
             t_esm_rank = float(args.filter_variant_esm_rank / 100.0)
             df_sub = df_sub[df_sub['esm_rank'] >= t_esm_rank]
 
-            # Filter on only WT > 80%
-            df_sub = df_sub[df_sub['wt_pIRS_rank'] >= args.filter_variant_pirs_rank]
-
             # Filter on delta < 0 (deimmunizing)
             if args.mode == "deimmunize":
-                df_sub = df_sub[df_sub['wt_pIRS_rank'] >= 80.0]
+                # Filter on only WT > 80%
+                df_sub = df_sub[df_sub['wt_pIRS_rank'] >= args.filter_variant_pirs_rank]
                 df_sub = df_sub[df_sub['delta'] < 0]
                 df_sub = df_sub.sort_values(by='weight', ascending=True)
                 print(f"\nDeimmunizing variants in range: {start}-{end} ({df_sub.shape[0]} available mutations)")
